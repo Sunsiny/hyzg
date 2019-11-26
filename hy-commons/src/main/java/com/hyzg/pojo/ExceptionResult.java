1e13bcc9b78d99318461ca544c2a4b14c1e99f33
tom_glb <goodMoring_pro@itluma.com>
2019-11-26 14:13:15 +0800

    后台授权呀

---
+++ hy-commons/src/main/java/com/hyzg/pojo/ExceptionResult.java
@@ -0,0 +1,21 @@
+package com.hyzg.pojo;
+
+
+import com.hyzg.enums.ExceptionEnums;
+import lombok.Data;
+
+ //封装异常的信息返回----给用户的
+@Data
+public class ExceptionResult {
+    private int status;
+    private String message;
+    //时间戳
+    private Long timestamp;
+    //构造方法，传的是枚举对象
+    public ExceptionResult(ExceptionEnums exceptionEnums){
+        this.status = exceptionEnums.getCode();
+        this.message = exceptionEnums.getMsg();
+        //当前时间戳
+        this.timestamp = System.currentTimeMillis();
+ }
+}
